7adf7610f61dc2d240a26c387bdc6f94d019ed01
beyzaerf <beyzaerfidan@gmail.com>
2023-04-23 20:42:32 +0300

    add php producer login

--- login.php
+++ login.php
@@ -7,6 +7,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if ($username === "c" && $password === "123") {
     header("Location: consumer_home.html");
     exit();
+  } 
+  else if ($username === "p" && $password === "123"){
+    header("Location: producer_home_page.html");
+    exit();
   } else {
     $error_msg = "Incorrect username or password";
   }
